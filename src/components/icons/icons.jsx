def8b499d449a49524555578d2054c397ff82b86
DevTeddy123 <ab315601@naver.com>
2024-07-18 15:22:41 +0900

    Design : icon 추가

--- src/components/icons/icons.jsx
+++ src/components/icons/icons.jsx
@@ -1,3 +1,19 @@
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { MdKeyboardDoubleArrowDown } from 'react-icons/md';
+import { MdKeyboardDoubleArrowUp } from 'react-icons/md';
 
 export const HamburgerIcon = () => <RxHamburgerMenu size={26} color="white" />;
+export const ArrowIcon = () => (
+    <MdKeyboardDoubleArrowDown
+        size={50}
+        color="white"
+        style={{ marginTop: 30, cursor: 'pointer' }}
+    />
+);
+export const UpArrowIcon = () => (
+    <MdKeyboardDoubleArrowUp
+        size={50}
+        color="white"
+        style={{ marginBottom: 30, cursor: 'pointer' }}
+    />
+);
