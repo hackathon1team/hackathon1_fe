d426f1974110f6d588a9efbc568664b3b7699853
DevTeddy123 <ab315601@naver.com>
2024-07-18 17:37:56 +0900

    Docs : 폴더구조 변경

--- src/pages/signUp/signUpPage.jsx
+++ src/pages/signUp/signUpPage.jsx
@@ -1,17 +1,21 @@
-import styled from 'styled-components'
-import Background from '../../assets/Img/backgroundImg/signUp_1.png'
-import { BackGroundImg } from '../../styles/common'
-import MetaAbout from './components/MetaAbout'
+import styled from 'styled-components';
+import Background from '../../assets/Img/backgroundImg/signUp_1.png';
+import Background1 from '../../assets/Img/backgroundImg/signUp_2.png';
+import { BackGroundImg } from '../../styles/common';
 
 function SignUpPage() {
-	return (
-		<BackImg>
-			<MetaAbout/>
-		</BackImg>
-	)
+    return (
+        <>
+            <BackImg></BackImg>
+            <BackImg1></BackImg1>
+        </>
+    );
 }
-export default SignUpPage
+export default SignUpPage;
 
 const BackImg = styled.div`
-	${BackGroundImg(Background)}
-`
+    ${BackGroundImg(Background)}
+`;
+const BackImg1 = styled.div`
+    ${BackGroundImg(Background1)}
+`;
