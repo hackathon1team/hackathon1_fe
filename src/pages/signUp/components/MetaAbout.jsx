f4020477a5582beff1c9cbe58cd83b84900571a5
Sumin893 <144125069+Sumin893@users.noreply.github.com>
2024-07-19 11:56:23 +0900

    Design: MetaAbout (메타인지란?) 페이지 화면크기에 맞게 수정함 (#11)
    * Design: MetaQuestion 디자인 완료
    
    * Design: MetaAbout (메타인지란?) 페이지 화면크기에 맞게 수정함

--- src/pages/signUp/components/MetaAbout.jsx
+++ src/pages/signUp/components/MetaAbout.jsx
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 const MetaAbout = () => {
     return (
-        <AboutMetacog>
+        <AboutMetaCog>
             <AboutMetaWord>
                 <h1>메타인지란?</h1>
-                <Metaword1>자기인식</Metaword1>
-                <Metaword2>자기평가</Metaword2>
-                <Metaword3>계획</Metaword3>
+                <MetaWord1>자기인식</MetaWord1>
+                <MetaWord2>자기평가</MetaWord2>
+                <MetaWord3>계획</MetaWord3>
             </AboutMetaWord>
             <AboutMetaDes>
                 <span>메타인지</span>는 자기 자신의 사고 과정에 대해 <br />
@@ -15,66 +15,68 @@ const MetaAbout = () => {
                 즉, 자신의 생각, 학습, 이해, 기억 등을 <br />
                 인식하고 조절하는 능력입니다.
             </AboutMetaDes>
-            <Metabutton>질문 대답 하러가기</Metabutton>
-        </AboutMetacog>
+            <MetaButton>질문 대답 하러가기</MetaButton>
+        </AboutMetaCog>
     );
 };
 
 export default MetaAbout;
 
-const AboutMetacog = styled.div``;
+const AboutMetaCog = styled.div`
+    padding-top: 18%;
+`;
 
 const AboutMetaWord = styled.div`
     h1 {
         font-size: 32px;
         color: white;
         text-align: center;
-        margin-top: 100px;
     }
 `;
 
-const Metaword1 = styled.div`
+const MetaWord1 = styled.div`
     font-size: 24px;
     font-weight: 700;
     color: #ffffffc7;
-    padding-top: 100px;
-    padding-left: 100px;
+    padding-top: 25%;
+    padding-left: 15%;
 `;
 
-const Metaword2 = styled.div`
+const MetaWord2 = styled.div`
     font-size: 24px;
     font-weight: 700;
     color: #ffffffc7;
-    padding-top: 70px;
-    padding-left: 300px;
+    padding-top: 8%;
+    padding-left: 60%;
 `;
 
-const Metaword3 = styled.div`
+const MetaWord3 = styled.div`
     font-size: 24px;
     font-weight: 700;
     color: #ffffffc7;
-    padding: 70px 0px 50px 150px;
+    padding-top: 8%;
+    padding-left: 25%;
 `;
 
 const AboutMetaDes = styled.div`
     text-align: left;
-    font-size: 18px;
+    font-size: 16px;
     font-weight: 600;
     color: #ffffffc7;
-    padding: 30px 110px;
+    padding: 15% 0 5% 10%;
 
     span {
-        font-size: 18px;
+        font-size: 16px;
         color: #444b77;
     }
 `;
 
-const Metabutton = styled.button`
+const MetaButton = styled.button`
     border: none;
     border-radius: 20px;
-    font-size: 15px;
+    font-size: 13px;
     color: #5a639c;
-    padding: 10px 20px;
-    font-weight: 600;
-    margin-left: 300px;
+    padding: 8px 12px;
+    font-weight: 700;
+    margin-left: 55%;
 `;
