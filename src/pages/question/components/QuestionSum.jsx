b3a4dc1cb76f49f414fd94fb681ad64fa63ad657
Sumin893 <ea0307@naver.com>
2024-07-23 02:37:07 +0900

    Feat: 메코의질문 정리 답변 map 구현

--- src/pages/question/components/QuestionSum.jsx
+++ src/pages/question/components/QuestionSum.jsx
@@ -1,42 +1,36 @@
 import styled from 'styled-components';
 import meco from '../../../assets/Img//questionImg/happy.png';
 const QuestionSum = () => {
+    // const contents = ['태기와 주먹다짐'];
+    const questions = [
+        '기분이 어땠나요?',
+        '그 행동의 결과는 어땠나요?',
+        '다음에는 어떻게 해결할 건가요?',
+    ];
+    const answers = [
+        '싸워서 마음이 아팠어요',
+        '나의 주먹이 날라갔어요',
+        '주먹보다 말로 풀려고 할 거에요',
+    ];
+
     return (
         <>
             <QuestionSumImg>
                 <img src={meco} alt="QuestionSumImg" />
             </QuestionSumImg>
             <QuestionAnswer>
-                <Wrapper2>
-                    <Question2>오늘의 인상깊은 사건은 무엇인가요?</Question2>
-                    <Answer2>친구들과의 약속</Answer2>
-                </Wrapper2>
-                <Wrapper2>
-                    <Question2>
-                        이 사건에서 느낀 감정은 무엇이고 원인은 무엇인가요?
-                    </Question2>
-                    <Answer2>
-                        오랜만에 친구들을 만나 너무나도 행복했답니다~ 오랜만에
-                        친구들을 만나 너무나도 행복했답니다~ 오랜만에 친구들을
-                        만나 너무나도 행복했답니다~ 오랜만에 친구들을 만나
-                        너무나도 행복했답니다~
-                    </Answer2>
-                </Wrapper2>
-                <Wrapper2>
-                    <Question2>
-                        이 감정이 내 행동에 어떻게 영향을 미치고 있나요?
-                    </Question2>
-                    <Answer2>
-                        친구들에게 “나 지금 너무 행복해” 라고 말했어요!
-                    </Answer2>
-                </Wrapper2>
-                <Wrapper2>
-                    <Question2>
-                        앞으로 비슷한 상황이 발생했을 때, 어떻게 대처하면
-                        좋을까요?
-                    </Question2>
-                    <Answer2>친구는 소중하다...................</Answer2>
-                </Wrapper2>
+                <Wrapper>
+                    <Question>오늘의 인상깊은 사건은 무엇인가요?</Question>
+                    <Answer>친구들과의 약속</Answer>
+                </Wrapper>
+                <>
+                    {questions.map((el, idx) => (
+                        <Wrapper>
+                            <Question>{questions[idx]}</Question>
+                            <Answer>{answers[idx]}</Answer>
+                        </Wrapper>
+                    ))}
+                </>
             </QuestionAnswer>
         </>
     );
@@ -44,10 +38,7 @@ const QuestionSum = () => {
 
 export default QuestionSum;
 
-const QuestionSumImg = styled.div`
-    display: flex;
-    justify-content: center;
-`;
+const QuestionSumImg = styled.div``;
 
 const QuestionAnswer = styled.div`
     text-align: left;
@@ -55,14 +46,14 @@ const QuestionAnswer = styled.div`
     color: #edececde;
     font-weight: 700;
 `;
-const Wrapper2 = styled.div`
+const Wrapper = styled.div`
     padding-bottom: 8%;
 `;
-const Question2 = styled.div`
+const Question = styled.div`
     padding-bottom: 2%;
     font-size: 18px;
 `;
-const Answer2 = styled.div`
+const Answer = styled.div`
     font-size: 16px;
     border-radius: 10px;
     background-color: #0000002b;
