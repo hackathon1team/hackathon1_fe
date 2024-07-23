46f485ffa7f0a883cf9d5bd3b5e08b73bf08cea6
Sumin893 <ea0307@naver.com>
2024-07-23 15:03:58 +0900

    Feat: 메코의질문 정리 map구현

--- src/pages/question/components/QuestionSum.jsx
+++ src/pages/question/components/QuestionSum.jsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
 import meco from '../../../assets/Img//questionImg/happy.png';
+
 const QuestionSum = () => {
-    // const contents = ['태기와 주먹다짐'];
-    const questions = [
-        '기분이 어땠나요?',
-        '그 행동의 결과는 어땠나요?',
-        '다음에는 어떻게 해결할 건가요?',
+    const Sum = [
+        {
+            contents: '태기와 주먹다짐',
+            questions: [
+                '기분이 어땠나요?',
+                '그 행동의 결과는 어땠나요?',
+                '다음에는 어떻게 해결할 건가요?',
+            ],
+            answers: [
+                '싸워서 마음이 아팠어요',
+                '나의 주먹이 날라갔어요',
+                '주먹보다 말로 풀려고 할 거에요',
+            ],
+        },
     ];
-    const answers = [
-        '싸워서 마음이 아팠어요',
-        '나의 주먹이 날라갔어요',
-        '주먹보다 말로 풀려고 할 거에요',
-    ];
+
+    const { questions, answers } = Sum[0];
 
     return (
         <>
@@ -23,14 +30,12 @@ const QuestionSum = () => {
                     <Question>오늘의 인상깊은 사건은 무엇인가요?</Question>
                     <Answer>친구들과의 약속</Answer>
                 </Wrapper>
-                <>
-                    {questions.map((el, idx) => (
-                        <Wrapper>
-                            <Question>{questions[idx]}</Question>
-                            <Answer>{answers[idx]}</Answer>
-                        </Wrapper>
-                    ))}
-                </>
+                {questions.map((question, Idx) => (
+                    <Wrapper key={Idx}>
+                        <Question>{question}</Question>
+                        <Answer>{answers[Idx]}</Answer>
+                    </Wrapper>
+                ))}
             </QuestionAnswer>
         </>
     );
